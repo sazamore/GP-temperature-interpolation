e7f147d732acdcf1168aefb4eadd66f0a6f3202d
Ricardo Decal <richard.decal@ncf.edu>
2014-12-08 18:39:31 -0800

    reorganized header, plot needs checkup
    better spacing, deleted backup file

--- gptempdata.py
+++ gptempdata.py
@@ -18,18 +18,19 @@ import scipy.io as io
 
 import os
 mydir = os.path.dirname(__file__)
+
+#pull out tempearture data 
 lhstore2 = os.path.join(mydir, "data", 'lhstore2.mat')
-lh50 = os.path.join(mydir, 'data', 'final-lh50.mat')
-
-data = io.loadmat(lhstore2)   #open data file (.mat)
-
-temperature = data['store2'].T    #pull out tempearture data 
+data = io.loadmat(lhstore2)
+temperature = data['store2'].T    
 T = temperature[:210,1,:]       #subset of data to work with - one height, removed unnecessary points
 
-data = io.loadmat(lh50)
-
-pos_mm = data['p_mm']   #pull out positional data
+#pull out positional data
+lh50 = os.path.join(mydir, 'data', 'final-lh50.mat')
+data = io.loadmat(lh50) #TODO: make data a unique name
+pos_mm = data['p_mm']
 time_averaged_temperature = data['s']      #time averaged temperature data
+
 
 x_observed = pos_mm[:15,0]           #x (crosswind) axis, observed data
 y_observed = np.unique(pos_mm[:,1])
@@ -43,7 +44,7 @@ T_reshaped =  np.reshape(T,(14,15,20000))  #reshape T for easier expansion of in
 T_observed = np.zeros([9,15])        #preallocate matrix. 
 
 for i in range(1,10):
-    T_observed[i-1,:] = time_averaged_temperature[1, i*15-15:i*15]   #get corresponding crosswind slice temperatures
+    T_observed[i-1,:] = time_averaged_temperature[1, i*15 - 15:i*15]   #get corresponding crosswind slice temperatures
 
 T_observed_mean = np.mean(T_observed, 0) - np.min(np.mean(T_observed, 0))     #subtract offset, improves fit
 
@@ -58,8 +59,7 @@ T_observed_mean = np.mean(T_observed, 0) - np.min(np.mean(T_observed, 0))     #s
 def gaus(x, A, mu, sigma):
     """"gaussian function, for fitting to distribution
     """
-    #A,mu,sigma = p
-    return A * np.exp(-(x-mu) ** 2 / (2. * sigma ** 2))
+    return A * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2))
 
     
 def gaus2(x, A1, mu1, sigma1, A2, mu2, sigma2):
@@ -70,26 +70,26 @@ def gaus2(x, A1, mu1, sigma1, A2, mu2, sigma2):
     
 #fit gaussian to distribution
 p0 = [1, 90, 15]   #start guess for fitting
-coeff1, cov = curve_fit(gaus, x_observed, T_observed_mean, p0=p0) #inputs can be: gaus,centers, dist,coeff_guess, if using temperature distribution data
-hist_fit = gaus(x_observed,*coeff1)
+coeff1, cov = curve_fit(gaus, x_observed, T_observed_mean, p0 = p0) #inputs can be: gaus,centers, dist,coeff_guess, if using temperature distribution data
+hist_fit = gaus(x_observed, *coeff1)
 
 T_observed_adjusted = T_observed_mean - hist_fit    #subtract out first gaussian, to fit second (if necessary)
 
 #fit second gaussian, if necessary 
 p0 = [0.12, 40, 5]
-coeff2, cov2 = curve_fit(gaus,x_observed, np.abs(T_observed_adjusted), p0= p0)   #not sure how I feel about abs val..
-hist_fit2 = gaus(x_observed,*coeff2)
+coeff2, cov2 = curve_fit(gaus,x_observed, np.abs(T_observed_adjusted), p0 = p0)   #not sure how I feel about abs val..
+hist_fit2 = gaus(x_observed, *coeff2)
 
 #create final coefficients and fits
-coeff = np.concatenate((coeff1, coeff2), axis=0)
+coeff = np.concatenate((coeff1, coeff2), axis = 0)
 T_fit = hist_fit + hist_fit2
 
 #plot to check fit
-#plt.plot(x_observed,T_observed_mean,'ro',label='Test data'), plt.plot(x_observed,hist_fit,label='Fitted data')
+plt.plot(x_observed,T_observed_mean,'ro',label='Test data'), plt.plot(x_observed,hist_fit,label='Fitted data')
 
 #prediction locations
 #x_predicted = np.atleast_2d(np.random.rand(100))*coeff(1)   #random data, around mean
-x_predicted = np.atleast_2d(np.linspace(0,254,50))       #2 mm prediction sites
+x_predicted = np.atleast_2d(np.linspace(0, 254, 50))       #2 mm prediction sites
 x_observed = np.atleast_2d(x_observed)    #make 2d for gaussian process fit. TODO: figure out what atleast_2d does
 
 T_observed = np.atleast_2d(T_observed)    #make 2d for gaussian process fit
