dc66cf7f74d70b7b40f44b6a652033320040a29a
sazamore <sharri@u.washington.edu>
2014-12-18 17:56:57 -0800

    only uses sum of gaussians
    removed individual gaussians (gaus, gaus2) replaced with sum of
    gaussians (gaus2 only)
    changes shape of x_observed
    added y_observed

--- gptempdata.py
+++ gptempdata.py
@@ -51,6 +51,8 @@ y_raw[13] = y_raw[1]      #last row repeats
 #IN PROGRESS: grid shape to data (x, y,temperature), to calculate std at each location
 
 x_observed, y_observed= np.meshgrid(x_raw, y_raw)
+x_observed = x_observed[0,:]
+y_observed = y_observed[0,:]
 T_observed =  np.reshape(temperatures_raw,(14,15,20000))  #reshape T for easier expansion of interpolation into 1+ dimensions
 
 T_time_avg = np.mean(T_observed,2)
@@ -65,34 +67,17 @@ T_sd = np.std(T_observed,2)
 #h2 = np.float(np.sum(h,0))  
 #dist = h/h2   #convert histogram to probability
 
-def gaus(x, A, mu, sigma):
-    """"gaussian function, for fitting to distribution
+def gaus(x, A1, mu1, sigma1, A2, mu2, sigma2):
+    """Sum of (2) gaussians, to fit crosswind distribution
     """
-    return A * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2))
-
-    
-def gaus2(x, A1, mu1, sigma1, A2, mu2, sigma2):
     return gaus(x, A1, mu1, sigma1) + gaus(x, A2, mu2, sigma2)
 
     
-#fit gaussian to distribution
-p0 = [1, 90, 15]   #start guess for fitting
-coeff1, cov = curve_fit(gaus, x_observed[0,:], T_observed_mean.T, p0 = p0) #inputs can be: gaus,centers, dist,coeff_guess, if using temperature distribution data
-T_fit1 = gaus(x_observed[0,:], *coeff1)
-
-
-T_observed_adjusted = T_observed_mean - T_fit1    #subtract out first gaussian, to fit second (if necessary)
-
-#fit second gaussian, if necessary 
-#TODO: make detector (if statement?) to determine if distribution is a second gaussian. Not sure how to do this.
-p0 = [0.12, 40, 5]
-coeff2, cov2 = curve_fit(gaus,x_observed[0,:], np.abs(T_observed_adjusted), p0 = p0)   #not sure how I feel about abs val..
-T_fit2 = gaus(x_observed[0,:], *coeff2)
-
-
-#create final coefficients and fits
-coeff = np.concatenate((coeff1, coeff2), axis = 0)
-T_fit = T_fit1 + T_fit2
+#fit sum of gaussians to distribution
+p0 = [1, 90, 15 ,
+      0.12, 35, 2]
+coeff, cov = curve_fit(gaus,x_observed,T_observed_mean,p0=p0)
+T_fit = gaus(x_observed,*coeff)
 
 #plot to check fit
 #plt.plot(x_observed,temp_observed_mean,'ro',label='Test data'), plt.plot(x_observed,histemp_fit,label='Fitted data')
@@ -102,13 +87,13 @@ T_fit = T_fit1 + T_fit2
 #prediction locations
 #x_predicted = np.atleast_2d(np.random.rand(100))*coeff(1)   #random data, around mean
 x_predicted = np.atleast_2d(np.linspace(0, 254, 50))       #2 mm prediction sites
-x_observed = np.atleast_2d(x_observed[0,:])    #make 2d for gaussian process fit. TODO: figure out what atleast_2d does
+x_observed = np.atleast_2d(x_observed)    #make 2d for gaussian process fit. TODO: figure out what atleast_2d does
 
 T_observed_mean = np.atleast_2d(T_observed_mean)    #make 2d for gaussian process fit
-T_observed_adjusted = np.atleast_2d(T_observed_adjusted)
 T_fit = np.atleast_2d(T_fit)
 
-   
+#nugget =  (T_sd/T_observed_mean)**2 
+  
 #TODO: make section into separate function
    
 #TODO: add optimizer, resize nugget
@@ -116,11 +101,11 @@ gp = gaussian_process.GaussianProcess(corr = 'absolute_exponential',
                                       theta0 = 1./25, 
                                       thetaL = None,
                                       thetaU = None)
-#                                      nugget = np.std(temp_observed,1)/temp_observed_mean)
+                                      #nugget = nugget)
 
 gp.fit(x_observed.T, T_fit.T)
 
 #y_expected_fit = gaus(x_observed,*coeff)     #single gaussian expected y values
-T_expected_fit = gaus2(x_observed, *coeff) #coeff)     #expected y values with double-gaussian-fit
+T_expected_fit = gaus(x_observed, *coeff) #coeff)     #expected y values with double-gaussian-fit
 T_prediction, y_prediction_MSE = gp.predict(x_predicted.T, eval_MSE = True)   #produce predicted y values
 sigma = np.sqrt(y_prediction_MSE)   #get SD of fit at each x_predicted location (for confidence interval)
