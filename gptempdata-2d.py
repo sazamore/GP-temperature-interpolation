626753af94f7da9b33348653a99990227f2b1dd8
sazamore <sharri@u.washington.edu>
2015-01-27 16:56:14 -0800

    runtime errors fixed, tweaked gp fit

--- gptempdata-2d.py
+++ gptempdata-2d.py
@@ -29,7 +29,7 @@ T_raw = lhstore2_data['store2'].T
 ### temperatures_raw.shape() ==>  (215, 4, 20000)
 
 #TODO: Allow for selection or incorporation of other heights (2nd dimension of temperatures_raw)
-T_raw = T_raw[:210,0,:]       #subset of data to work with - one height, removed unnecessary points at end of wind tunnel
+T_raw = T_raw[:210,1,:]       #subset of data to work with - one height, removed unnecessary points at end of wind tunnel
 
 #pull out positional data
 lh50_file = os.path.join(mydir, 'data', 'final-lh50.mat')
@@ -58,29 +58,6 @@ T_sd = np.std(T_raw,1)
 #fit all of the observed data into one array, for ease of use of fitting function
 observed_data[:2,:] = xy_observed
 observed_data[2,:] = T_time_avg
-    
-def lin_gaus_2d(xy_observed, A1, mu_x1, sigma_x1, mu_y1, sigma_y1, A2, mu_x2, sigma_x2, mu_y2, sigma_y2, m, b):
-    """sum of two 2-dimensional gaussians + y-dimension linear slope
-    """
-    return A1 * np.exp(-((xy_observed[0]-mu_x1)**2/(2.*sigma_x1**2) + 
-    (xy_observed[1]-mu_y1)**2/(2.*sigma_y1**2))) + A2 * np.exp(-((xy_observed[0]-mu_x2)**2/(2.*sigma_x2**2)+
-    (xy_observed[1]-mu_y2)**2/(2.*sigma_y2**2))) + (xy_observed[1]*m + b)
-    
-#initial paramater values for fit function
-p0 = [1, 69, 1, 500,5,
-         1.5, 100, 1, 500, 5,
-         1/800, 0]
-
-coeff, cov = curve_fit(lin_gaus_2d, xy_observed, observed_data[2,:]-np.mean(observed_data[2,:]), p0 = p0)
-T_fit = lin_gaus_2d(xy_observed, *coeff)
-
-#convert such that this stupid gp can actually understand what's going on (dimension matching)
-T_fit_2d = np.zeros([2,len(T_fit.T)])
-T_fit_2d[0,:] = T_fit
-T_fit_2d[1,:] = T_fit
-
-#plot to check fit
-#plt.plot(x_observed,temp_observed_mean,'ro',label='Test data'), plt.plot(x_observed,histemp_fit,label='Fitted data')
 
 #TODO: move everything below this to a function and/or separate script
 
@@ -93,30 +70,27 @@ x1,x2 = np.meshgrid(x_predicted, y_predicted)
 xy_predicted = np.vstack([x1.reshape(x1.size), x2.reshape(x2.size)]).T
 
 #calculate noise (required)
-nugget =  T_sd/T_time_avg)**2
+nugget =  (T_sd/T_time_avg)**2
 nugget = nugget[:181]       #deletes repeated measurment locations
   
 #TODO: make section into separate function
    
-#TODO: add optimizer, resize nugget
 gp = gaussian_process.GaussianProcess(corr = 'absolute_exponential',
                                       theta0 = 1./25, 
-                                      thetaL = None,
-                                      thetaU = None,
+                                      thetaL = 1e-2,
+                                      thetaU = 1,
+                                      normalize = True,
+                                      random_start = 100,
                                       nugget = nugget)
-#TODO: nugget array size error
-#nugget has to be at least 2d
 
-
-gp.fit(xy_observed[:181,:], T_time_avg[:181])
-   
+gp.fit(xy_observed.T[:181,:], T_time_avg[:181])
+#gp.fit(xy_observed.T[:181,:], T_raw[:181,:])  #with time variants, this fits each time step...non ideal.
+ 
 #Target value error will come up with that last repeated row. It can't have 
 #multiple measurements at the same location. Consider deleting that repeated
 #last row of measurements or take a mean or stack the timeseries onto the 
 #first measurement, which will effectivly average the values.
 
-#y_expected_fit = gaus(x_observed,*coeff)     #single gaussian expected y values
-#T_expected_fit = lin_gaus_2d(xy_observed, *coeff) #coeff)     #expected y values with double-gaussian-fit
 T_prediction, y_prediction_MSE = gp.predict(xy_predicted, eval_MSE = True)   #produce predicted y values
 sigma = np.sqrt(y_prediction_MSE)   #get SD of fit at each x_predicted location (for confidence interval)
 
